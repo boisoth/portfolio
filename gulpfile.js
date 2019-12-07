d4b126124964d5221bac85d709e80b479e6ca1d8
Boi Soth <naneang415@gmail.com>
2019-12-06 16:40:04 -0800

    adding gulp task runner and such

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const { src, dest, parallel } = require("gulp");
+const sass = require("gulp-sass");
+const minify = require("gulp-csso");
+
+function css() {
+  return src("src/sass/main.scss")
+    .pipe(sass())
+    .pipe(dest("build/css"));
+}
+
+exports.default = css;
