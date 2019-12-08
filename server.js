98906254961e3975982003b9441604503b5d0209
Boi Soth <naneang415@gmail.com>
2019-12-07 23:18:40 -0800

    seperating html views

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ const app = express();
 const path = require("path");
 const http = require("http").createServer(app);
 const PORT = process.env.PORT || 5000;
+const htmlRoutes = require("./routes/html-routes");
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+app.use(htmlRoutes);
 
 http.listen(PORT, () => console.log(`Listening on ${PORT}`));
