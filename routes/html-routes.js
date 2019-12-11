68f2463c21e0eef424883a36cc0a754c5978381a
Boi Soth <naneang415@gmail.com>
2019-12-11 14:53:32 -0800

    head tag title changes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,15 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("home", { path: "/" });
+  res.render("home", { path: "/", title: "About" });
 });
 
 router.get("/resume", (req, res) => {
-  res.render("resume", { path: "/resume" });
+  res.render("resume", { path: "/resume", title: "Resume" });
 });
 
 router.get("/contact", (req, res) => {
-  res.render("contact", { path: "/contact" });
+  res.render("contact", { path: "/contact", title: "Contact" });
 });
 
 module.exports = router;
