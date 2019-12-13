d325040d2ce3af6fdffe26a9668f2adef3bdd0bd
Boi Soth <naneang415@gmail.com>
2019-12-12 22:05:17 -0800

    changing port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const http = require("http").createServer(app);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const htmlRoutes = require("./routes/html-routes");
 
 app.set("view engine", "pug");
